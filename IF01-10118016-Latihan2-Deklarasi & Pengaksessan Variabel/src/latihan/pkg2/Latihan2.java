45fea59e0c584c1259a85d24577f05eab901b0d7
rezkynurfauzi <55779378+rezkynurfauzi@users.noreply.github.com>
2019-09-26 12:28:21 +0700

    Add files via upload

---
+++ IF01-10118016-Latihan2-Deklarasi & Pengaksessan Variabel/src/latihan/pkg2/Latihan2.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan.pkg2;
+
+/**
+ *
+ * Nama : Rezky Nur Fauzi
+ * NIM  : 10118016  
+ * Kelas: IF-1 
+ * Deskripsi Program : Pendeklarasian variabel di Java
+ */
+public class Latihan2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //Deklarasi variabel
+        int nilaiInt;
+        final double PHI = 3.14 //Konstanta
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        //Memberi nilai ke variabel
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        //Menampilkan hasil
+        System.out.println();
+        System.out.println("Isi variabel nilai = " + nilaiInt);
+        System.out.println("Isi variabel PHI = " + PHI);
+        System.out.println("Isi variabel logika = " + nilaiLogika);
+        System.out.println("Isi variabel karakter = " + nilaiKarakter);
+    }
+    
+}
